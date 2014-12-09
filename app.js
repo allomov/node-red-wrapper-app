7c2cd71d827fbf7bca98de95e4e04be50ed7732a
A.S. Lomoff <lomov.as@gmail.com>
2014-12-09 18:20:34 +0300

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var express = require("express");
+
+var RED = require("node-red");
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
+
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    uiPort: process.env.PORT || 1880,
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(settings['uiPort'] || 1880);
+
+// Start the runtime
+RED.start();
